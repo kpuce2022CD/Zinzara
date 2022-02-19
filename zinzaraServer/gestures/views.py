64db3146ab0fb0d9fa53fcbae6e124a7087b11c1
hkcho01 <hkcho01@naver.com>
2022-02-19 15:36:52 +0900

    gesture base64

--- zinzaraServer/gestures/views.py
+++ zinzaraServer/gestures/views.py
@@ -46,68 +46,27 @@ def find_gradient(a1,a2,b1,b2):
 def gestures(request):
     data = JSONParser().parse(request)
     img = data["img"]
-    print("aaa")
-    # img = img + "=" * (4 - len(img) % 4)
-    # img = base64.b64decode(img)
-    # print("bbb")
-    # dataBytesIO = io.BytesIO(img)
-    # print("ccc")
-    # image = Image.open(dataBytesIO)
-    # print("ddd")
-    # image = cv2.cvtColor(np.array(img))
-    # print("eee")
-
-
-    # img = img + "=" * (4 - len(img) % 4)
-    # print("bbb")
-    # img = base64.b64decode(img)
-    # print("ccc")
-    # dataBytesIO = io.BytesIO(img)
-    # print("ddd")
-    # image = Image.open(dataBytesIO)
-    # print("eee")
-    # image = cv2.cvtColor(np.array(image))
-    # print("fff")
-
 
     img = img + "=" * (4 - len(img) % 4)
+    img = base64.b64decode(img)
+    dataBytesIO = io.BytesIO(img)
+    image = Image.open(dataBytesIO)
 
-    imgData = base64.b64decode(str(img))
-    print("aaa")
-    img = Image.open(io.BytesIO(imgData))
-    print("bbb")
-    image = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-    print("ccc")
+    image = cv2.cvtColor(np.array(image), cv2.IMREAD_COLOR)   # color
+    #image = cv2.cvtColor(np.array(image), cv2.IMREAD_GRAYSCALE)   # grayscale
 
     mp_drawing = mp.solutions.drawing_utils
     mp_hands = mp.solutions.hands
-    with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.7) as hands:
-        #image = cv2.imread("50.jpg", cv2.IMREAD_COLOR)
-
-        #image3 = request.GET.get("img")
-
-        #print(image3)
-        #image = np.ndarray(image3, dtype=np.uint8)
-        print("success")
-        print(type(image))
-        print(image.dtype)
-        print(image.shape)
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.flip(image, 1)
-        print(image.shape)
+    with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.3, min_tracking_confidence=0.3) as hands:
+        #image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)   # grayscale
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   # color
+        # image = cv2.flip(image, 1)
         results = hands.process(image)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         arr = np.array([])
 
-        print("000")
-
         if results.multi_hand_landmarks:
-            print("zzz")
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-            print("111")
 
             for i in range(1, 20):
                 if i == 4:
@@ -161,20 +120,37 @@ def gestures(request):
                 float(find_gradient(int(hand_landmarks.landmark[17].x * 255), int(hand_landmarks.landmark[17].y * 255),
                                     int(hand_landmarks.landmark[20].x * 255), int(hand_landmarks.landmark[20].y * 255)))))
 
-            print("222")
-
             arr = arr.reshape(-1, 40)
 
             tmp = model.predict(arr)
             predicted = np.argmax(tmp, axis=1)
             print(predicted)
+            predicted = int(predicted)
             arr = np.array([])
-            print("333")
-            return HttpResponse(status=580)
+            if predicted == 0:
+                return HttpResponse(status=210)
+            elif predicted == 1:
+                return HttpResponse(status=211)
+            elif predicted == 2:
+                return HttpResponse(status=212)
+            elif predicted == 3:
+                return HttpResponse(status=213)
+            elif predicted == 4:
+                return HttpResponse(status=214)
+            elif predicted == 5:
+                return HttpResponse(status=215)
+            elif predicted == 6:
+                return HttpResponse(status=216)
+            elif predicted == 7:
+                return HttpResponse(status=217)
+            elif predicted == 8:
+                return HttpResponse(status=218)
+            elif predicted == 9:
+                return HttpResponse(status=219)
 
         else:
-            print("vvv")
-            return HttpResponse(status=590)
+            print("fail")
+            return HttpResponse(status=220)
 
 
 
