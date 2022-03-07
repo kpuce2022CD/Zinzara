1527ed0a5ae898da196cc7a1d3cb21b26407c0fb
rkskek1226 <hkcho01@naver.com>
2022-03-07 18:49:31 +0900

    physical_rehabilitation table

--- DB/쿼리.sql
+++ DB/쿼리.sql
@@ -4,10 +4,7 @@ CREATE TABLE `members` (
 	`phone_number` VARCHAR(13) NOT NULL COLLATE 'utf8_general_ci',
 	`created` DATETIME(6) NOT NULL,
 	PRIMARY KEY (`user_id`) USING BTREE
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+);
 
 
 CREATE TABLE `devices` (
@@ -19,8 +16,15 @@ CREATE TABLE `devices` (
 	PRIMARY KEY (`idx`) USING BTREE,
 	INDEX `devices_user_id_9a5cca49` (`user_id`) USING BTREE,
 	CONSTRAINT `devices_user_id_9a5cca49_fk_members_user_id` FOREIGN KEY (`user_id`) REFERENCES `zinzara`.`members` (`user_id`) ON UPDATE RESTRICT ON DELETE RESTRICT
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=11
-;
+);
+
+
+CREATE TABLE `physical_rehabilitation` (
+	`physical_score` INT(11) NOT NULL,
+	`rehabilitation_time` DATETIME(6) NOT NULL,
+	`idx` INT(11) NOT NULL AUTO_INCREMENT,
+	`user_id` VARCHAR(30) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`idx`) USING BTREE,
+	INDEX `physical_rehabilitation_user_id_dec6d569` (`user_id`) USING BTREE,
+	CONSTRAINT `physical_rehabilitation_user_id_dec6d569_fk_members_user_id` FOREIGN KEY (`user_id`) REFERENCES `zinzara`.`members` (`user_id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
